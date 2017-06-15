e05abe4b83166d39b15e974bf3d13a81801b514a
joulis1derful <joulis1derful@gmail.com>
2017-06-16 00:01:58 +0300

    Configured base project

---
+++ src/main/java/com/joulis1derful/remindme/server/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.joulis1derful.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.joulis1derful.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
---
+++ src/main/java/com/joulis1derful/remindme/server/controller/ReminderController.java
@@ -0,0 +1,17 @@
+package com.joulis1derful.remindme.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/reminder")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public @ResponseBody String getReminder(ModelMap model) {
+        return "My reminder";
+    }
+}
